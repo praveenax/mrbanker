a66c0490edd40dfb8d37bba33cb6a7f39b166b0d
Prax <xav.axp@gmail.com>
2016-09-23 19:15:30 +0530

    onclick not working

--- client/js/script.js
+++ client/js/script.js
@@ -2,63 +2,76 @@
 
 $(document).ready(function () {
 
-        	 var socket = io.connect();
-			 socket.on('chat-resp', function (data){
-			 	console.log(data);
+    function view(param) {
+        console.log(param);
+    }
 
-			 	var type = data["type"];
+    $(".chat-btn").on("click", function (data) {
+        console.log(data);
+    });
 
-			 	if(type==2){
+    var socket = io.connect();
+    socket.on('chat-resp', function (data) {
+        console.log(data);
 
-			 		var link_items = data["links"];
+        var type = data["type"];
 
-			 		var link_html_content = "";
-			 		// $("#chatArea").prepend("<div class='link_box'>");
-			 		link_html_content = link_html_content + "<div class='hi_msg'>"+data["msg"]+"</div><br><div class='link_box'><div class='msgDiv'>";
+        if (type == 2) {
 
-			 		for(item in link_items){
-			 			console.log(link_items[item]);
-			 			link_html_content = link_html_content + "<a target='blank' href='\serve?q='"+link_items[item]["node_id"]+">"+link_items[item]["node_name"]+"" + "<br>";
-	                	// $("#chatArea").prepend("<div class='msgDiv'><a target='blank' href="+link_items[item]["url"]+">"+link_items[item]["name"]+"</div>" + "<br>");	
-			 		}
-			 		// $("#chatArea").prepend("</div>");
-			 		link_html_content = link_html_content + "</div></div>";
+            var link_items = data["links"];
 
-			 		$("#chatArea").prepend(link_html_content);
-			 		$("#sendButton").click();
-			 	}else{
-				 	 $("#chatArea").prepend("<div class='msgDiv' >"+$("#chatInput").val() + "</div><br><br>");
-				 	 $("#sendButton").click();
-			 	}
-			    console.log(data["msg"]);
+            var link_html_content = "";
+            // $("#chatArea").prepend("<div class='link_box'>");
+            link_html_content = link_html_content + "<div class='hi_msg'>" + data["msg"] + "</div><br><div class='link_box'><div class='msgDiv'>";
 
-			    
+            for (item in link_items) {
+                console.log(link_items[item]);
+                // var node_id_tmp = "'"+ link_items[item]["node_id"]+"'";
+                var node_id_tmp = link_items[item]["node_id"];
+                var node_name_tmp = link_items[item]["node_name"];
+                link_html_content = link_html_content + "<input type='button' class='btn btn-info chat-btn' onclick=view('" + node_id_tmp + "')  value='" + node_name_tmp + "'  /><br>";
+                // $("#chatArea").prepend("<div class='msgDiv'><a target='blank' href="+link_items[item]["url"]+">"+link_items[item]["name"]+"</div>" + "<br>");	
+            }
+            // $("#chatArea").prepend("</div>");
+            link_html_content = link_html_content + "</div></div>";
 
-			    // socket.emit('my other event', { my: 'data' });
-			 });
+            $("#chatArea").prepend(link_html_content);
+            $("#sendButton").click();
+        } else {
+            $("#chatArea").prepend("<div class='msgDiv' >" + $("#chatInput").val() + "</div><br><br>");
+            $("#sendButton").click();
+        }
+        console.log(data["msg"]);
 
-            $("#chatInput").focus();
 
-            $("#sendButton").on("click", function () {
 
-            	
+        // socket.emit('my other event', { my: 'data' });
+    });
 
-                // $("#chatArea").prepend("<div class='msgDiv' >"+$("#chatInput").val() + "</div><br><br>");
-                $("#chatInput").val("");
+    $("#chatInput").focus();
 
+    $("#sendButton").on("click", function () {
+
+        // $("#chatArea").prepend("<div class='msgDiv' >"+$("#chatInput").val() + "</div><br><br>");
+        $("#chatInput").val("");
+
+    });
+
+    $("#chatInput").keypress(function (event) {
+
+        var key = event.which;
+        if (key == 13) {
+
+            socket.emit('chat', {
+                msg: '' + $("#chatInput").val()
             });
 
-            $("#chatInput").keypress(function (event) {
 
-                var key = event.which;
-                if (key == 13) {
+        }
+        //115
+    });
 
-                	socket.emit('chat', { msg: ''+$("#chatInput").val() });
-                    
 
-                }
-                //115
-            });
 
-            
+
 });
--- data.js
+++ data.js
@@ -11,15 +11,58 @@ var decision_tree = [
 	{	
 		node_id:"p1",
 		node_name:"Transaction",
+		child_nodes:[
+			{	
+				node_id:"p1c1",
+				node_name:"Security password correct "
+			},
+			{	
+				node_id:"p1c2",
+				node_name:"Security Wrong password"
+			},
+			
+		]
 	},{
 		node_id:"p2",
 		node_name:"Withdraw",
+		child_node:[
+			{	
+				node_id:"p2c1",
+				node_name:"Correct password"
+			},
+			{	
+				node_id:"p1c2",
+				node_name:"Wrong password"
+			}
+		]
+			
 	},{
 		node_id:"p3",
 		node_name:"Bank Balance",
+		child_node:[
+			{	
+				node_id:"p3c1",
+				node_name:"Correct password"
+			},
+			{	
+				node_id:"p3c2",
+				node_name:"Wrong password"
+			}
+			]
+			
 	},{
 		node_id:"p4",
 		node_name:"Value Added Service",
+		child_node:[
+			{	
+				node_id:"p4c1",
+				node_name:"Personal"
+			},
+			{	
+				node_id:"p4c2",
+				node_name:"Business"
+			}
+			]
 	}
 ];
 
