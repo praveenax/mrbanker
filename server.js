76cecd33ea882b83a39e150639867c3126faa22b
Prax <xav.axp@gmail.com>
2016-09-24 02:26:25 +0530

    commented unwanted code;fixed the undefined msg object

--- server.js
+++ server.js
@@ -31,16 +31,11 @@ app.get('/chatMsg', function (req, res) {
 
 
 io.on('connection', function (socket) {
-    // console.log('a user connected');
-    // io.emit('chat', {
-    //     msg: "TestMsg"
-    // });
-
+    
     properEmit = function(event_name,event_msg,event_links){
     	io.emit(event_name, {
 					type:2,
 			        msg: event_msg,
-			        // links:[{name:"Google",url:"http://www.google.com"},{name:"Facebook",url:"http://www.facebook.com"},{name:"Github",url:"http://www.github.com"}]
 			        links:event_links
 
 		});
@@ -48,22 +43,14 @@ io.on('connection', function (socket) {
 
 
     socket.on('chat', function (data) {
-    	// console.log(data);
-
+    
     	var inp_chat_str = data["msg"];
 
     	if(inp_chat_str == "@banker"){
 
     		properEmit('chat-resp',"Hi. I am Mr.Banker. What can I help you with?",decision_tree["data"]);
 
-			// io.emit('chat-resp', {
-			// 		type:2,
-			//         msg: "Hi. I am Mr.Banker. What can I help you with?",
-			//         // links:[{name:"Google",url:"http://www.google.com"},{name:"Facebook",url:"http://www.facebook.com"},{name:"Github",url:"http://www.github.com"}]
-			//         links:decision_tree["data"]
-
-			//  });
-    	}else{
+		}else{
     		io.emit('chat-resp', {
     			type:1,
         		msg: ""+data["msg"]
@@ -73,19 +60,18 @@ io.on('connection', function (socket) {
     	
   	});
 
-  	//node_event
-
+  	
   	 socket.on('node_event', function (data) {
-    	// console.log(data);
-
-    	var inp_chat_str = data["msg"];
+    
+    	var inp_chat_str = data["nodeId"];
+		console.log(data);
+    	console.log(inp_chat_str);
     	var tmp_arr = [];
     	var tmp_node_name = "";
 
     	var data_arr = decision_tree["data"];
 
-    	// console.log(data_arr);
-
+    
     	//find unique id from the tree
 
     	for(item in data_arr){
@@ -95,37 +81,15 @@ io.on('connection', function (socket) {
     			tmp_arr = tmp_obj["child_node"];
     			tmp_node_name = tmp_obj["node_name"];
 
-
-
     			break;
     		}
 
 
     	}
 
-    	// _.each(decision_tree["data"],function(node){
-
-    	// 	if(node["node_id"] == inp_chat_str){
-    	// 		tmp_arr = node["child_node"];
-    	// 		tmp_node_name = node["node_name"];
-
-    	// 	}
-
-    	// });
-
     	properEmit('chat-resp',"Your Choice: "+tmp_node_name,tmp_arr);
 
-  //   	io.emit('chat-resp', {
-		// 	type:2,
-	 //        msg: "Your Choice: "+tmp_node_name,
-	 //        // links:[{name:"Google",url:"http://www.google.com"},{name:"Facebook",url:"http://www.facebook.com"},{name:"Github",url:"http://www.github.com"}]
-	 //        links:tmp_arr
-
-		// });
-
-
-    	
-
+  
     	
   	});
 
